7121dd71c2badc109d250825da3d0166161abe37
ias_design <verena.koller92@gmail.com>
2026-06-12 13:29:00 +0200

    Austria: serve small images in grid, full-res for download

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // IAS Weekends Gallery - v2
 const galleries = {
-  austria2024:    { folder: 'photos/austria2024',    count: 42 },
+  austria2024:    { folder: 'photos/austria2024',    count: 42, fullFolder: 'photos/austria2024/full' },
   portugal2025:   { folder: 'photos/portugal2025',   count: 62 },
   switzerland2025:{ folder: 'photos/switzerland2025',count: 66 },
   greece2026:     { folder: 'photos/greece2026',     count: 257, fullFolder: 'photos/greece2026/full' }
